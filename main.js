9215f4c64dcbe3cd5dd7266b6e559b28401866fc
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-06-30 08:49:22 +0200

    add random number generator and input listener

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('Hola');
+'use strict';
+
+const randomNumber = getRandomNumber(100);
+const numberInput = document.querySelector('.js-numberInput');
+const button = document.querySelector('.js-button');
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+console.log(`El número aleatorio es ${randomNumber}`);
+
+function handleNumberInputChange(event) {
+  const numberInputValue = event.target.value;
+  console.log(numberInputValue);
+}
+
+function handleButtonClick(event) {
+  event.preventDefault();
+  console.log('hola');
+}
+
+numberInput.addEventListener('keyup', handleNumberInputChange);
+button.addEventListener('click', handleButtonClick);
